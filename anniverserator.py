547cd8394c3a4ecfdeb8286f12ee99cfef497938
OyeInyalowda <msvance18@gmail.com>
2025-11-17 22:59:50 -0500

    added saving and loading functionality

--- anniverserator.py
+++ anniverserator.py
@@ -12,17 +12,18 @@ Anniverserator, never forget how long you've been married again!
 Anniverserator is a small python program that will hopefully one day be a small command line utility for persistent tracking of important dates. 
 
 Author: Mike Vance
-Version: 0.2
+Version: 0.3
 """
 
 # done create an Event class which holds a title and date
 # done parse user provided dates
 # done calculate time elapsed from date to current time
-# TODO save user provided event to file
-# TODO read user provided event from file
+# done save user provided event to file
+# done read user provided event from file
 # TODO create_event() does not handle leap years properly
 # TODO create_event does not handle string inputs for months
 # done calculate time until next anniversary
+# todo functionality to delete events
 
 class Event:
     """Event consisting of a title and date"""
@@ -34,7 +35,7 @@ class Event:
     def print_title(self):
         """print event title"""
 
-        print(f"********** {self.eventName} **********")
+        print(f"\n********** {self.eventName} **********")
 
     def print_elapsed_days(self):
         """print the days ellapsed between the event and today"""
@@ -78,17 +79,31 @@ class Event:
 #---------- End Event Class ----------#
 
 #TODO the param for this should be an array of events
-def save(event):
+def save(events) -> bool:
     try:
         with open("events.pickle", "wb") as file:
-            pickle.dump(event, file, protocol=pickle.HIGHEST_PROTOCOL)
+            pickle.dump(events, file, protocol=pickle.HIGHEST_PROTOCOL)
+        file.close()
+        print("\nSaving...")
+        return True
     except Exception as ex:
         print("Error while pickling object", ex)
+        return False
 
-    return #TODO return bool
+def load(filename: str) -> list:
+    result = [] 
+    try:
+        with open(filename, "rb") as file:
+            result = pickle.load(file)
+        file.close()
+        print("\nLoading...")
+    except Exception as ex:
+        print("Error while unpickling object", ex)
 
-def create_event() -> Event:
-    """Create a new event from user input"""
+    return result
+
+def create_events(events: list) -> list :
+    """Create new events from user input and return a list of those events"""
 
     print("**** Create New Anniverserator Event ****")
     print("*   Valid inputs:                       *")                    
@@ -98,8 +113,16 @@ def create_event() -> Event:
     print("*       Day: 0 < Day <= # days in month *")
     print("*****************************************")
 
-    response = ''
-    while(response != 'y'):
+    correct = False
+    anotherEvent = False
+
+    # user input loop
+    while(anotherEvent or not correct):
+        title = ""
+        year = -69
+        month = -6
+        day = -9
+
         #get title
         title = input("Event title? ")
 
@@ -129,39 +152,63 @@ def create_event() -> Event:
                 dayStr = input(f"Invalid day. Please enter a value between in range for the given month: ")
                 day = int(dayStr)
 
-        # ask if correct
+        # ask if correct and add to list if so
         print(f"\nEvent Title: {title} | Event Date: {eventDate}")
-        response = input("Is this correct (y/n)? ")
-    
-    newEvent = Event(title, eventDate)
-    return newEvent
+         
+        if input("Is this correct (y/n)? ") == 'y':
+            correct = True
+            events.append(Event(title, eventDate))
+
+            # ask for another event
+            if(input("Would you like to add another event (y/n)? ") == 'y' ):
+                anotherEvent = True
+            else:
+                anotherEvent = False
+        else:
+            correct = False
+
+    return events
 
 def main():
+    # check for existing events
+    filename = "events.pickle"
+    loadedEvents = load(filename)
+
+    if(not loadedEvents):
+        existingEvents = False
+        events = []
+    else:
+        existingEvents = True
+        events = loadedEvents
+
     # args and options
     description = "Anniverserator, never forget how long you've been married again!"
     parser = argparse.ArgumentParser(description)
     parser.add_argument("-n", "--New",
-                        action = 'store_true', 
-                        help  = "Create a new event.")
+                        action = "store_true", 
+                        help  = "Create new events.")
     parser.add_argument("-p", "--Print",
-                        action = 'store_true', 
+                        action = "store_true", 
                         help  = "Print event facts")
 
     #parse args
     args = parser.parse_args()    
 
     if (args.New):
-        event = create_event()
-    else:
-        event = Event()
+        events = create_events(events)
+        existingEvents = True
+        save(events)
 
-    #print
-    if(args.Print):
-        event.print_title()
-        event.print_elapsed_years()
-        event.print_elapsed_days()
-        event.print_next_occurrence()
-
+    if(args.Print and existingEvents):
+        for event in events: 
+            event.print_title()
+            event.print_elapsed_years()
+            event.print_elapsed_days()
+            event.print_next_occurrence()
+    elif(args.Print and not existingEvents):
+        print("Uh oh :( No events to print")
+        print("Try running Anniverserator again with the -n flag to add new events!")
+        
 if __name__ == "__main__":
     main()
 
